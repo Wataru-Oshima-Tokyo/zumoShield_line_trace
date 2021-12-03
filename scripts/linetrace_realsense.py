72ae13a04b3f0bb0cfa9d0d079eb14793a484647
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-12-02 23:24:55 -0800

    Update linetrace_realsense.py

--- scripts/linetrace_realsense.py
+++ scripts/linetrace_realsense.py
@@ -61,7 +61,7 @@ class Follower:
 		mask[search_bot:h, 0:w] = 0
 
 		self.M = cv.moments(mask)    #maskにおける1の部分の重心
-		if cg['m00'] > 0:    #重心が存在する
+		if self.M['m00'] > 0:    #重心が存在する
 			self.cx = int(self.M['m10']/self.M['m00']) #重心のx座標
 			self.cy = int(self.M['m01']/self.M['m00']) #重心のy座標
 			cv.circle(image, (self.cx, self.cy), 20, (0, 0, 255), -1) #赤丸を画像に描画
