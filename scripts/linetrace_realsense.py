8a9ed44db23ba8e391a78bebca75210c1ae07c78
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-12-02 19:00:17 -0800

    Update linetrace_realsense.py

--- scripts/linetrace_realsense.py
+++ scripts/linetrace_realsense.py
@@ -9,35 +9,37 @@ import numpy as np
 from geometry_msgs.msg import Twist
 
 """
-M : 与える操作量
-M1 : 一つ前に与えた操作量
-e : 偏差(目的値と現在値の差)
-e1 : 前回の偏差
-e2 : 前々回の偏差
-Kp : 比例制御（P制御)の比例定数
-Ki : 積分制御（I制御)の比例定数
-Kd : 微分制御（D制御)の比例定数
+#M : 与える操作量
+#M1 : 一つ前に与えた操作量
+#e : 偏差(目的値と現在値の差)
+#e1 : 前回の偏差
+#e2 : 前々回の偏差
+#Kp : 比例制御（P制御)の比例定数
+#Ki : 積分制御（I制御)の比例定数
+#Kd : 微分制御（D制御)の比例定数
 """
 
-global M = 0.00 
-global M1 =  0.00   
-global e = 0.00 
-global e1 = 0.00 
-global e2 = 0.00 
-global Kp = 0.1 
-global Ki = 0.1 
-global Kd = 0.1 
+
 
 class Follower:
 	def __init__(self):
 		print("__init__")
 		self.bridge = cv_bridge.CvBridge()
+		self.M = 0.00
+		self.M1 =  0.00
+		self.e = 0.00
+		self.e1 = 0.00
+		self.e2 = 0.00
+		self.Kp = 0.1
+		self.Ki = 0.1
+		self.Kd = 0.1
 #		cv.namedWindow('BGR Image', 1)  #'BGR Image'という名前の画像表示のウィンドウを作成
 #		cv.namedWindow('MASK', 1)   #'MASK'という名前の画像表示のウィンドウを作成
 #		cv.namedWindow('MASKED', 1) #'MASK'という名前の画像表示のウィンドウを作成
-		self.image_sub = rospy.Subscriber('camera/color/image_raw', Image, self.image_callback)   #Image型で画像トピックを購読し，コールバック関数を呼ぶ
+		self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)   #Image型で画像トピックを購読し，コールバック関数を呼ぶ
 		self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
 		self.twist = Twist()    #Twistインスタンス生成
+		
 	def image_callback(self, msg):
 		#print("I will write down codes below")
 		image = self.bridge.imgmsg_to_cv2(msg, desired_encoding = 'bgr8')
@@ -56,10 +58,10 @@ class Follower:
 		mask[0:search_top, 0:w] = 0
 		mask[search_bot:h, 0:w] = 0
 
-		M = cv.moments(mask)    #maskにおける1の部分の重心
-		if M['m00'] > 0:    #重心が存在する
-			cx = int(M['m10']/M['m00']) #重心のx座標
-			cy = int(M['m01']/M['m00']) #重心のy座標
+		cg = cv.moments(mask)    #maskにおける1の部分の重心
+		if cg['m00'] > 0:    #重心が存在する
+			cx = int(cg['m10']/cg['m00']) #重心のx座標
+			cy = int(cg['m01']/cg['m00']) #重心のy座標
 			cv.circle(image, (cx, cy), 20, (0, 0, 255), -1) #赤丸を画像に描画
 
 		err = cx - w//2 #黄色の先の重心座標(x)と画像の中心(x)との差
@@ -85,15 +87,16 @@ class Follower:
 
 	def PIDcontrol(self, goal):
 		t = 100
-		self.twist.linear.x = 0.002
-		global M, M1, e,e1,e2
-			for i in range(t):
-			M1 = M
-			e2 = e1
-			e1 = e
-			M =  M1 + Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
-
-			self.twist.linear.z = M
+		self.twist.linear.x = 1
+		for i in range(t):
+			self.M1 = self.M
+			self.e2 = self.e1
+			self.e1 = self.e
+			self.M =  self.M1 + self.Kp * (self.e-self.e1) + self.Ki * self.e + self.Kd * ((self.e-self.e1) - (self.e1-self.e2))
+			
+			
+# 			rospy.loginfo("Linear: " + str(self.twist.linear.x) + " Angular " + str(self.twist.angular.z))
+			self.twist.angular.z = self.M
 			self.cmd_vel_pub.publish(self.twist)
 		
 #Unnecessary but it will be  used in the future--------------
