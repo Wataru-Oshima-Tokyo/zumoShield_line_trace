f1bf83e0b7636484064ae5afdf32244f1607aa65
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-12-03 18:42:28 -0800

    Update linetrace_realsense.py

--- scripts/linetrace_realsense.py
+++ scripts/linetrace_realsense.py
@@ -36,9 +36,9 @@ class Follower:
 		self.cx =0
 		self.cy = 0
 		self.first = True
-                cv.namedWindow('BGR Image', 1)  #'BGR Image'という名前の画像表示のウィンドウを作成
-                cv.namedWindow('MASK', 1)   #'MASK'という名前の画像表示のウィンドウを作成
-                cv.namedWindow('MASKED', 1) #'MASK'という名前の画像表示のウィンドウを作成
+#                 cv.namedWindow('BGR Image', 1)  #'BGR Image'という名前の画像表示のウィンドウを作成
+#                 cv.namedWindow('MASK', 1)   #'MASK'という名前の画像表示のウィンドウを作成
+#                 cv.namedWindow('MASKED', 1) #'MASK'という名前の画像表示のウィンドウを作成
 		self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)   #Image型で画像トピックを購読し，コールバック関数を呼ぶ
 		self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
 		self.twist = Twist()    #Twistインスタンス生成
@@ -75,15 +75,15 @@ class Follower:
 
 		#大きすぎるため，サイズ調整
 		#print("大きすぎるため，サイズ調整")
-		display_mask = cv.resize(mask, RESIZE)
-		display_masked = cv.resize(masked, RESIZE)
-		display_image = cv.resize(image, RESIZE)
+# 		display_mask = cv.resize(mask, RESIZE)
+# 		display_masked = cv.resize(masked, RESIZE)
+# 		display_image = cv.resize(image, RESIZE)
 
 		#表示
 		#print("表示")
-                cv.imshow('BGR Image', display_image)   #'BGR Image'ウィンドウにimageを表示
-                cv.imshow('MASK', display_mask)         #'MASK'ウィンドウにimageを表示
-                cv.imshow('MASKED', display_masked)     #'MASKED'ウィンドウにimageを表示
+#                 cv.imshow('BGR Image', display_image)   #'BGR Image'ウィンドウにimageを表示
+#                 cv.imshow('MASK', display_mask)         #'MASK'ウィンドウにimageを表示
+#                 cv.imshow('MASKED', display_masked)     #'MASKED'ウィンドウにimageを表示
 		#cv.setMouseCallback("HSV", self.mouseEvent)
 		cv.waitKey(3)   #3秒待つ
 
