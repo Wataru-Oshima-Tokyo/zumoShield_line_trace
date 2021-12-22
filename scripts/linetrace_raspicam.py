141c3c8d5a1898f879ac42787ddc228f0627dc60
Your Name <you@example.com>
2021-12-22 02:13:25 +0000

    fixed some bugs

--- scripts/linetrace_raspicam.py
+++ scripts/linetrace_raspicam.py
@@ -67,13 +67,16 @@ class Follower:
 			cv.circle(image, (self.cx, self.cy), 20, (0, 0, 255), -1) #赤丸を画像に描画
 
 			err = self.cx - w//2 #黄色の先の重心座標(x)と画像の中心(x)との差
-	# 		self.twist.linear.x = 0.2
-	#  		self.twist.angular.z = -float(err)/2000 #誤差にあわせて回転速度を変化させる（-1/1000がP制御でいうところの比例ゲインにあたる）
+			self.twist.linear.x = 0.2
+			self.twist.angular.z = -float(err)/2000 #誤差にあわせて回転速度を変化させる（-1/1000がP制御でいうところの比例ゲインにあたる）
 			#self.cmd_vel_pub.publish(self.twist)
-			self.PIDcontrol(err)
-
-		#大きすぎるため，サイズ調整
+			#self.PIDcontrol(err)
+		else:
+                    self.twist.linear.x = 0.0
+                    self.twist.angular.z = 0.0
+                    #大きすぎるため，サイズ調整
 		#print("大きすぎるため，サイズ調整")
+		self.cmd_vel_pub.publish(self.twist)
 		display_mask = cv.resize(mask, RESIZE)
 		display_masked = cv.resize(masked, RESIZE)
 		display_image = cv.resize(image, RESIZE)
